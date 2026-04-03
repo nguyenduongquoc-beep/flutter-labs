14eb93aa249b1a3a358f5554a9484ec9ea99da01
nguyenduongquoc <nguyenduongquoc9a11@gmail.com>
2026-04-03 20:03:54 +0700

    upload all lab1

---
+++ lab1_1_2224802010878_nguyenduongquoc/android/app/src/main/kotlin/com/example/lab1_1_2224802010878_nguyenduongquoc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab1_1_2224802010878_nguyenduongquoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab1_2_2224802010878_nguyenduongquoc/test/lab1_2_2224802010878_nguyenduongquoc_test.dart
@@ -0,0 +1,8 @@
+import 'package:lab1_2_2224802010878_nguyenduongquoc/lab1_2_2224802010878_nguyenduongquoc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
